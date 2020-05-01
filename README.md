0aa8211c8fa4da1b24b4d0ca90eaa3ed8bd48b1b
ZC Zhong <zzhong@wehkamp.nl>
2020-05-01 14:45:32 +0200

    add logger

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # image-segmentation
+
+
+### ToDo:
+1. Merge the two-soft branch to master
+2. Edit the example notebooks
+3. Finish the readme:  
+    a. Descriptions
+    b. Explain the algorithm
+    c. Include examples and benchmark
--- seglib/utils.py
+++ seglib/utils.py
@@ -1,6 +1,38 @@
-
 from sklearn.metrics import jaccard_score
-import numpy as np
+import mrcnn.model as modellib
+from mrcnn.config import Config
+import logging
+
+logger = logging.getLogger('image-seg')
+#-------------------
+# for mask-rcnn
+#-------------------
+def load_model(weight_path, maskrcnn_config=None, model_dir='./models'):
+    """Load mask-rcnn models from a h5 file.
+    :params
+        weight_path:
+    """
+    # Define the config is not defined
+    if not maskrcnn_config:
+        class InferenceConfig(Config):
+            NAME = 'segmentation'
+            GPU_COUNT = 1
+            IMAGES_PER_GPU = 1
+            NUM_CLASSES = 1 + 1
+            IMAGE_MIN_DIM = 128
+            IMAGE_MAX_DIM = 512
+
+        inference_config = InferenceConfig()
+    else:
+        inference_config = maskrcnn_config
+
+    inference_model = modellib.MaskRCNN(mode="inference",
+                                        config=inference_config,
+                                        model_dir=model_dir)
+    logger.info(f"Loading weights from {weight_path}")
+    inference_model.load_weights(weight_path, by_name=True)
+    return inference_model
+
 
 # ------------------------
 # Evaluation Metrics
